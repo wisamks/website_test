0528639a91ba6172ca95330240ce65e901488527
AI_11_김재영A <goodikjy@naver.com>
2024-05-13 12:16:28 +0900

    고양이 추가

--- makingImage.js
+++ makingImage.js
@@ -1,10 +1,29 @@
-const dogImage = document.getElementById('dog');
+const dog = document.getElementById('dog');
 const makingImageBtn = document.getElementById('makingImage');
 
 const getImage = async e => {
   e.preventDefault();
-  const res = await fetch('https://dog.ceo/api/breeds/image/random');
-  const data = await res.json();
-  dogImage.src = data.message;
+  let res;
+  let data;
+  let source;
+  if (e.target.id === 'makingCat') {
+    res = await fetch('https://api.thecatapi.com/v1/images/search');
+    data = await res.json();
+    source = data[0].url;
+  } else if (e.target.id === 'makingDog') {
+    res = await fetch('https://dog.ceo/api/breeds/image/random');
+    data = await res.json();
+    source = data.message;
+  }
+  
+  if (!dog.children[0]) {
+    const dogImage = document.createElement('img');
+    dogImage.src = source;
+    dogImage.style.width = "700px";
+    dogImage.style.height = "700px";
+    dog.appendChild(dogImage);
+  } else {
+    dog.firstElementChild.src = source;
+  }
 };
 makingImageBtn.addEventListener('click', getImage);
